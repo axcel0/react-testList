2a5459eb03ac53cd9d66b07e7024f19532df5020
Axel Santoso <axcelsantoso0@gmail.com>
2023-12-28 15:08:09 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,45 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useCallback } from 'react'
 import Table from 'react-bootstrap/Table';
 import './App.css'
 
 export default function App() {
   const [listPeople, setListPeople] = useState([]);
   const [editingIndex, setEditingIndex] = useState(-1);
-  const [editedName, setEditedName] = useState("");
+  const [editedPerson, setEditedPerson] = useState({});
 
-  useEffect(() => {
-    fetch("https://randomuser.me/api/?results=5")
-      .then((response) => response.json())
-      .then((data) => {
-        return setListPeople(data.results);
-      });
+  const fetchPeople = useCallback(async (url) => {
+    const response = await fetch(url);
+    const data = await response.json();
+    return data.results;
   }, []);
 
+  useEffect(() => {
+    fetchPeople("https://randomuser.me/api/?results=5")
+      .then(results => setListPeople(results));
+  }, [fetchPeople]);
+
   const deleteItem = (indexParam) => {
-    const newList = listPeople.filter((el, index) => {
-      return index !== indexParam;
-    });
-    setListPeople(newList);
+    setListPeople(prevList => prevList.filter((_, index) => index !== indexParam));
   };
 
   const getPeople = () => {
-    fetch("https://randomuser.me/api/?results=1")
-      .then((response) => response.json())
-      .then((data) => {
-        const newData = [...listPeople];
-        newData.push(data.results[0]);
-        console.log(data.results);
-        setListPeople(newData);
-      });
+    fetchPeople("https://randomuser.me/api/?results=1")
+      .then(result => setListPeople(prevList => [...prevList, ...result]));
   };
 
   const startEditing = (index) => {
     setEditingIndex(index);
-    setEditedName(listPeople[index].name.first);
+    setEditedPerson(listPeople[index]);
   };
 
   const saveEditedName = () => {
-    const newList = [...listPeople];
-    newList[editingIndex].name.first = editedName;
-    setListPeople(newList);
+    setListPeople(prevList => prevList.map((person, index) => index === editingIndex ? editedPerson : person));
     setEditingIndex(-1);
-    setEditedName("");
+    setEditedPerson({});
+  };
+
+  const handleInputChange = (field, value) => {
+    setEditedPerson(prevPerson => ({ ...prevPerson, [field]: value }));
   };
 
   return (
@@ -53,8 +49,9 @@ export default function App() {
           <tr>
             <th>Num</th>
             <th>Name</th>
-            <th>Country</th>
+            <th>Email</th>
             <th>Phone</th>
+            <th>Gender</th>
             <th>Action</th>
           </tr>
         </thead>
@@ -66,15 +63,46 @@ export default function App() {
                 {editingIndex === index ? (
                   <input
                     type="text"
-                    value={editedName}
-                    onChange={(e) => setEditedName(e.target.value)}
+                    value={editedPerson.name?.first}
+                    onChange={(e) => handleInputChange('name.first', e.target.value)}
                   />
                 ) : (
                   el.name.first
                 )}
               </td>
-              <td>{el.location.country}</td>
-              <td>{el.phone}</td>
+              <td>
+                {editingIndex === index ? (
+                  <input
+                    type="text"
+                    value={editedPerson.email}
+                    onChange={(e) => handleInputChange('email', e.target.value)}
+                  />
+                ) : (
+                  el.email
+                )}
+              </td>
+              <td>
+                {editingIndex === index ? (
+                  <input
+                    type="text"
+                    value={editedPerson.phone}
+                    onChange={(e) => handleInputChange('phone', e.target.value)}
+                  />
+                ) : (
+                  el.phone
+                )}
+              </td>
+              <td>
+                {editingIndex === index ? (
+                  <input
+                    type="text"
+                    value={editedPerson.gender}
+                    onChange={(e) => handleInputChange('gender', e.target.value)}
+                  />
+                ) : (
+                  el.gender
+                )}
+              </td>
               <td>
                 {editingIndex === index ? (
                   <button onClick={saveEditedName}>Save</button>
